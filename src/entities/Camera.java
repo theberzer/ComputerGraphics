c4f6c1db8fc4aed218212071b118c836c1557c67
Kim <Kim.brusevold@hiof.no>
2017-11-29 15:10:11 +0100

    Added functionality so that pressing escape shows the cursor and makes it possible to exit out. clicking the right mouse button in the window gives control back to the user.

--- src/entities/Camera.java
+++ src/entities/Camera.java
@@ -15,21 +15,40 @@ public class Camera {
 
 	private Vector3f position = new Vector3f(10, 10, 50);
 	private float pitch, yaw, roll;
+	private boolean focusWindow;
 
 	public Camera() {
-
+		focusWindow = true;
+		Mouse.setGrabbed(focusWindow);
 	}
 
 	public void move() {
-
+		
 		// This code was found in a comment during a binge on youtube and helps
 		// with the camera movement
-		float arg_yaw = Mouse.getDX();
-		System.out.println(arg_yaw);
-		yaw += arg_yaw / 10;
-		float arg_roll = Mouse.getDY();
-		pitch += -(arg_roll / 10);
-		Mouse.setGrabbed(true);
+		
+		//code for grabbing and freeing mouse is not from youtube @KimB
+		//free mouse from window if the escape key is pressed, or grab mouse if user want to focus window again
+		if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
+			focusWindow = false;
+			Mouse.setGrabbed(focusWindow);
+		}		
+		
+		//regrabbs 
+		if(focusWindow == false && Mouse.isButtonDown(0)) {
+			focusWindow = true;
+			Mouse.setGrabbed(focusWindow);
+		}
+		
+		//Turns off camera movement if the cursor is freed from the window
+		if(focusWindow == true) {
+			float arg_yaw = Mouse.getDX();
+			System.out.println(arg_yaw);
+			yaw += arg_yaw / 10;
+			float arg_roll = Mouse.getDY();
+			pitch += -(arg_roll / 10);
+		}
+		
 		// Forward
 		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
 			float toZ = ((float) Math.sin(Math.toRadians(yaw + 90)));
@@ -66,6 +85,8 @@ public class Camera {
 		if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
 			position.y -= 0.2f;
 		}
+		
+		
 	}
 
 	public Vector3f getPosition() {
